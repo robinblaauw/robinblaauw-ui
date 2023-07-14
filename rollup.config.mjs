a4eb19a06b91a9767f7940f2a170fce5c132e679
Robin Blaauw <robin@softwarebastards.nl>
2023-07-14 16:55:38 +0200

    added jest to project

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,9 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import packageJson from "./package.json" assert { type: "json" };
+import terser from "@rollup/plugin-terser";
 
 export default [
   {
@@ -21,9 +23,11 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
     ],
   },
   {
